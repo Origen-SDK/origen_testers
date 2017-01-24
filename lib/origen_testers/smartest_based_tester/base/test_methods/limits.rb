7f062c86af01c75fe19024a3ad87be608e5246bb
Ronnie Lajaunie <Ronnie.Lajaunie@freescale.com>
2017-01-24 14:00:04 -0600

    Add test number to limits section for functional test.

--- lib/origen_testers/smartest_based_tester/base/test_methods/limits.rb
+++ lib/origen_testers/smartest_based_tester/base/test_methods/limits.rb
@@ -6,6 +6,7 @@ module OrigenTesters
           attr_reader :test_method
           attr_accessor :lo_limit, :hi_limit
           attr_accessor :unit
+          attr_accessor :tnum
           alias_method :lo, :lo_limit
           alias_method :lo=, :lo_limit=
           alias_method :hi, :hi_limit
@@ -13,6 +14,7 @@ module OrigenTesters
 
           def initialize(test_method)
             @test_method = test_method
+            @tnum = ""
           end
 
           def unit=(val)
@@ -28,13 +30,17 @@ module OrigenTesters
 
           def to_s
             if !lo_limit && !hi_limit
-              "\"#{test_name}\"" + ' = "":"NA":"":"NA":"":"":""'
+              if tnum == ""
+                "\"#{test_name}\"" + ' = "":"NA":"":"NA":"":"":""'
+              else
+                "\"#{test_name}\"" + " = \"\":\"NA\":\"\":\"NA\":\"\":\"#{tnum}\":\"0\""
+              end
             elsif !lo_limit
-              "\"#{test_name}\"" + " = \"\":\"NA\":\"#{hi_limit}\":\"LE\":\"#{unit}\":\"\":\"0\""
+              "\"#{test_name}\"" + " = \"\":\"NA\":\"#{hi_limit}\":\"LE\":\"#{unit}\":\"#{tnum}\":\"0\""
             elsif !hi_limit
-              "\"#{test_name}\"" + " = \"#{lo_limit}\":\"GE\":\"\":\"NA\":\"#{unit}\":\"\":\"0\""
+              "\"#{test_name}\"" + " = \"#{lo_limit}\":\"GE\":\"\":\"NA\":\"#{unit}\":\"#{tnum}\":\"0\""
             else
-              "\"#{test_name}\"" + " = \"#{lo_limit}\":\"GE\":\"#{hi_limit}\":\"LE\":\"#{unit}\":\"\":\"0\""
+              "\"#{test_name}\"" + " = \"#{lo_limit}\":\"GE\":\"#{hi_limit}\":\"LE\":\"#{unit}\":\"#{tnum}\":\"0\""
             end
           end
 
