06087fd8773e55b4729e3c1295c4d69a93e7fe24
Stephen McGinty <stephen.f.mcginty@gmail.com>
2018-03-30 16:02:13 +0100

    Include namespace in inline pattern calls

--- lib/origen_testers/smartest_based_tester/smt8.rb
+++ lib/origen_testers/smartest_based_tester/smt8.rb
@@ -96,7 +96,7 @@ module OrigenTesters
         if comment =~ /^SQPG/
           write_gen_vec
           if comment =~ /^SQPG JSUB ([^;]+);/
-            @program_lines << "    <Instruction id=\"patternCall\" value=\"patterns.#{Regexp.last_match(1)}\"/>"
+            @program_lines << "    <Instruction id=\"patternCall\" value=\"#{tester.package_namespace}.patterns.#{Regexp.last_match(1)}\"/>"
           elsif comment =~ /^SQPG MACT (\d+);/
             @program_lines << "    <Instruction id=\"match\" value=\"#{Regexp.last_match(1)}\"/>"
           elsif comment =~ /^SQPG MRPT (\d+);/
