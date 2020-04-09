b83e55c00e30ff277e0526d0b7265cdee02c2db7
info-rchitect <brian.caquelin@outlook.com>
2020-04-09 14:58:35 -0500

    enable passing in Camelcase test methods

--- lib/origen_testers/smartest_based_tester/base/test_methods/base_tml.rb
+++ lib/origen_testers/smartest_based_tester/base/test_methods/base_tml.rb
@@ -12,6 +12,11 @@ module OrigenTesters
           end
 
           def method_missing(method, *args, &block)
+            tml_class_name = self.class.name.to_s.split('::')[-1]
+            # This enables passing camel case test methods for dc_tml and ac_tml
+            unless tml_class_name == 'CustomTml'
+              method = method.to_s.underscore.to_sym if camelcase?(method)
+            end
             if definitions[method]
               m = platform::TestMethod.new methods: definitions[method].dup,
                                            attrs:   (args.first || {}),
@@ -30,6 +35,12 @@ module OrigenTesters
 
           def definitions
             @definitions || self.class::TEST_METHODS
+          end
+
+          private
+
+          def camelcase?(test_method)
+            test_method.to_s.camelcase.to_sym == test_method ? true : false
           end
         end
       end
