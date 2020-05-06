ae15e41658a2097b0b1b7b35c97e346f76dbe657
Priyavadan Kumar <priyavadan.kumar@amd.com>
2020-05-06 15:32:44 -0500

    Changes as suggested by info-architect in his PR

--- program/flow_control.rb
+++ program/flow_control.rb
@@ -521,4 +521,13 @@ Flow.create interface: 'OrigenTesters::Test::Interface', flow_name: "Flow Contro
     func :read1, id: :ta5, bin: 10, number: 60000
     func :erase1, if_all_sites_passed: :ta5, bin: 12, number: 60010
   end
+
+  if tester.v93k?
+    loop from: 1, to: 5, step: 1, var: "loop" do
+      func :loop_check, test_number: 60001
+    end
+    loop from: 1, to: 5, step: 1 do
+      func :loop_check_no_var, test_number: 60002
+    end
+  end
 end
