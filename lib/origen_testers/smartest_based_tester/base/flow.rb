da4c60dd9dd5d1bff660d208d8c8d070ea508033
info-rchitect <brian.caquelin@outlook.com>
2020-05-06 13:05:58 -0500

    lint

--- lib/origen_testers/smartest_based_tester/base/flow.rb
+++ lib/origen_testers/smartest_based_tester/base/flow.rb
@@ -271,13 +271,13 @@ module OrigenTesters
           loop_flag = node.children[3] if node.children[3].is_a?(String)
           loop_end_str = loop_flag.nil? ? loop_end.to_s : "@#{loop_flag}"
           line "for @index = 0; @index < #{loop_end_str} ; @index = @index + #{loop_inc}; do"
-          line "test_number_loop_increment = 0"
-          line "{"
+          line 'test_number_loop_increment = 0'
+          line '{'
           @indent += 1
           children_start_index = loop_flag.nil? ? 3 : 4
           process_all(node.children[children_start_index..-1])
           @indent -= 1
-          line "}"
+          line '}'
         end
 
         def on_test(node)
