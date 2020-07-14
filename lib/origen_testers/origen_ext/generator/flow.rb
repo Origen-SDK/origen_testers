3df074ac285118f5a000479ba6b61d2cbc38946d
info-rchitect <brian.caquelin@outlook.com>
2020-07-14 15:56:36 -0500

    first attempt (unsuccesful) at inserting flow header

--- lib/origen_testers/origen_ext/generator/flow.rb
+++ lib/origen_testers/origen_ext/generator/flow.rb
@@ -183,6 +183,85 @@ module Origen
         [flow_comments, comments]
       end
 
+      def header
+        Origen.tester.pre_header if Origen.tester.doc?
+        inject_separator
+        if $desc
+          c2 'DESCRIPTION:'
+          $desc.split(/\n/).each { |line| cc line }
+          inject_separator
+        end
+        c2 'GENERATED:'
+        c2 "  Time:    #{Origen.launch_time}"
+        c2 "  By:      #{Origen.current_user.name}"
+        c2 "  Mode:    #{Origen.mode}"
+        binding.pry
+        l = "  Command: origen p #{job.requested_pattern} -t #{Origen.target.file.basename}"
+        if Origen.environment && Origen.environment.file
+          l += " -e #{Origen.environment.file.basename}"
+        end
+        c2(l)
+        inject_separator
+        c2 'ENVIRONMENT:'
+        c2 '  Origen'
+        c2 '    Source:    https://github.com/Origen-SDK/origen'
+        c2 "    Version:   #{Origen.version}"
+        unless Origen.app.plugins.empty?
+          c2 '  Plugins'
+          Origen.app.plugins.sort_by { |p| p.name.to_s }.each do |plugin|
+            c2 "    #{plugin.name}:".ljust(30) + plugin.version
+          end
+        end
+        inject_separator
+
+        unless Origen.app.plugins.empty?
+          # Plugins can use config.shared_flow_header to inject plugin-specific comments into the flow header
+          header_printed = false
+          Origen.app.plugins.sort_by { |p| p.name.to_s }.each do |plugin|
+            unless plugin.config.shared_flow_header.nil?
+              unless header_printed
+                c2 'Header Comments From Shared Plugins:'
+                header_printed = true
+              end
+              inject_flow_header(
+                config_loc:      plugin,
+                scope:           :shared_flow_header,
+                message:         "Header Comments From Shared Plugin: #{plugin.name}:",
+                message_spacing: 2,
+                line_spacing:    4,
+                no_separator:    true
+              )
+            end
+          end
+          inject_separator if header_printed
+        end
+
+        if Origen.app.plugins.current && !Origen.app.plugins.current.config.send(:current_plugin_flow_header).nil?
+          # The top level plugin (if one is set) can further inject plugin-specific comment into the header.
+          # These will only appear if the plugin is the top-level plugin though.
+          inject_flow_header(
+            config_loc: Origen.app.plugins.current,
+            scope:      :current_plugin_flow_header,
+            message:    "Header Comments From The Current Plugin: #{Origen.app.plugins.current.name}:"
+          )
+        end
+
+        unless Origen.app.config.send(:application_flow_header).nil?
+          inject_flow_header(
+            config_loc: Origen.app,
+            scope:      :application_flow_header,
+            message:    "Header Comments From Application: #{Origen.app.name}:"
+          )
+        end
+
+        if Origen.config.flow_header
+          Origen.log.deprecated 'Origen.config.flow_header is deprecated.'
+          Origen.log.deprecated 'Please use config.shared_flow_header, config.application_flow_header, or config.current_plugin_flow_header instead.'
+          inject_separator
+        end
+        Origen.tester.close_text_block if Origen.tester.doc?
+      end
+
       private
 
       def reload_target?(interface, options)
@@ -200,6 +279,7 @@ module Origen
           false
         end
       end
+
     end
   end
 
