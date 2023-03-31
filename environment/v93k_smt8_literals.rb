8d8f200ccd5c39e45109128d87a061e1121caac3
Daniel Hadad <daniel.hadad@nxp.com>
2023-03-31 14:58:59 -0700

    Updates to support option to include flow file name in flag names.

--- environment/v93k_smt8_literals.rb
+++ environment/v93k_smt8_literals.rb
@@ -6,5 +6,6 @@ tester.literal_flag_options = {
   type_first: true,
   fail_name: 'f',
   pass_name: 'p',
-  using_f_p_instead_FAILED_PASSED: true
+  using_f_p_instead_FAILED_PASSED: true,
+  flag_included_flow_name: true
 }
