80ab842391ff006a2ca074b681572c8dbd78ac4e
ikokjyan <ikokjyan@hotmail.com>
2025-10-29 11:17:03 +0800

    Enable regression for disable test number loop increment

---
+++ approved/v93k_disable_test_loop_increment/testflow/mfh.testflow.setup/flow_control_vars.tf
@@ -0,0 +1,102 @@
+hp93000,testflow,0.1
+language_revision = 1;
+
+declarations
+
+@AF11_BEA7F3B_FAILED = 0;
+@AF1_BEA7F3B_FAILED = 0;
+@AF2_BEA7F3B_FAILED = 0;
+@AT22_BEA7F3B_FAILED = 0;
+@Alarm = 0;
+@ECT2_1_BEA7F3B_FAILED = 0;
+@ECT2_2_BEA7F3B_FAILED = 0;
+@FIVE = 0;
+@FIVE_PNT_TWO = 0;
+@FLAG1 = 0;
+@GRP1_BEA7F3B_FAILED = 0;
+@GT_GRP1_BEA7F3B_FAILED = 0;
+@GT_GRP2_BEA7F3B_FAILED = 0;
+@IFA1_BEA7F3B_FAILED = 0;
+@IFA2_BEA7F3B_FAILED = 0;
+@IFALL1_BEA7F3B_FAILED = 0;
+@IFALLB1_BEA7F3B_FAILED = 0;
+@IFALLP1_BEA7F3B_PASSED = 0;
+@IFALLPB1_BEA7F3B_PASSED = 0;
+@IFAP1_BEA7F3B_PASSED = 0;
+@IFAP2_BEA7F3B_PASSED = 0;
+@IFAPB1_BEA7F3B_PASSED = 0;
+@IFAPB2_BEA7F3B_PASSED = 0;
+@JOB = "";
+@L1T5_BEA7F3B_FAILED = 0;
+@LT_VARIABLE = 0;
+@MY_FLAG = 0;
+@MY_INOUT_ADD_FLAG = 0;
+@MY_UNCALLED_FLAG = 0;
+@My_Mixed_Flag = 0;
+@OF11_BEA7F3B_FAILED = 0;
+@OF12_BEA7F3B_FAILED = 0;
+@OF1_BEA7F3B_FAILED = 0;
+@OF2_BEA7F3B_FAILED = 0;
+@OOF_PASSCODE1_BEA7F3B_FAILED = 0;
+@OOF_PASSCODE2_BEA7F3B_FAILED = 0;
+@SOME_FLAG = 0;
+@T5_BEA7F3B_RAN = 0;
+@T6_BEA7F3B_RAN = 0;
+@T7_BEA7F3B_RAN = 0;
+@T8_BEA7F3B_RAN = 0;
+@global = 0;
+@my_unset_flag = 0;
+@non_global = 0;
+
+end
+-----------------------------------------------------------------
+flags
+
+user BITMAP = 0;
+user CZ = 0;
+user EN1 = 0;
+user EN2 = 0;
+user EWORD1 = 0;
+user EWORD2 = 0;
+user EXTRAS = 0;
+user LOOP_VARIABLE = 0;
+user LOOP_VARIABLE1 = 0;
+user LOOP_VARIABLE2 = 0;
+user LOOP_VARIABLE3 = 0;
+user LT_VARIABLE = 0;
+user MCEn_extras = 0;
+user MCEn_test = 0;
+user MY_ENABLE_WORD = 0;
+user NVM_MINIMUM_COLD = 0;
+user NVM_MINIMUM_FT = 0;
+user NVM_MINIMUM_HOT = 0;
+user NVM_MINIMUM_ROOM = 0;
+user QUICK = 0;
+user SMALL_FLOW = 0;
+user WORD1 = 0;
+user WORD2 = 0;
+user WORD3 = 0;
+
+end
+-----------------------------------------------------------------
+testmethodparameters
+end
+-----------------------------------------------------------------
+testmethodlimits
+end
+-----------------------------------------------------------------
+test_flow
+end
+-----------------------------------------------------------------
+binning
+end
+-----------------------------------------------------------------
+oocrule
+end
+-----------------------------------------------------------------
+context
+end
+-----------------------------------------------------------------
+hardware_bin_descriptions
+end
+-----------------------------------------------------------------
